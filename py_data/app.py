dcd0d0237287c0c2228a5b889c80feb273e23128
HerbBoy <github@spsec.net>
2022-06-02 22:24:26 -0600

    changed port to 80

--- py_data/app.py
+++ py_data/app.py
@@ -48,9 +48,9 @@ def read_external_file():
 
 if __name__ == '__main__':
     logging.basicConfig(filename='/var/log/demo.log',level=logging.DEBUG)
-    HOST = environ.get('SERVER_HOST', 'localhost')
+    HOST = environ.get('SERVER_HOST', '0.0.0.0')
     try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
+        PORT = int(environ.get('SERVER_PORT', '80'))
     except ValueError:
-        PORT = 5555
+        PORT = 80
     app.run(HOST, PORT)

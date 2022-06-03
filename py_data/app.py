2ad2d7225a9b55155f020615daac61ed95fa34bc
HerbBoy <github@spsec.net>
2022-06-02 20:22:56 -0600

    implemented proper error handling of no null input

--- py_data/app.py
+++ py_data/app.py
@@ -6,6 +6,8 @@ from flask import request
 from flask import jsonify
 from flask import Response
 from flask import send_from_directory
+from flask import abort
+# from flask import render_template
 
 from os import environ
 
@@ -14,17 +16,19 @@ app = Flask(__name__)
 @app.route('/manage_file', methods = ['GET'])
 def manage_files():
     content = request.get_json()
-    if content['action'] == 'download':
+
+    if content == None:
+        abort(500)
+    elif content['action'] == 'download':
         get_external_file()
-        return send_from_directory(directory='', path='', filename='sample-text-file.txt')
+        return send_from_directory(directory='', path='', filename='sample-text-file.txt', download_name='sample-text-file.txt', as_attachment=True)
     elif content['action'] == "read":
         sample_content = read_external_file()
         return jsonify(sample_content), 200
-    else:
-        return Response(
-            "Invalid Request",
-            status=400
-        )
+
+# @app.errorhandler(500)
+# def internal_error(error):
+#     return render_template('500.html'), 500
 
 def get_external_file():
     url = 'https://www.learningcontainer.com/wp-content/uploads/2020/04/sample-text-file.txt'
@@ -49,4 +53,4 @@ if __name__ == '__main__':
         PORT = int(environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
-    app.run(HOST, PORT, debug=True)
+    app.run(HOST, PORT)

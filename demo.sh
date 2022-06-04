361262593db4c9fba1ba18f3605469b071940968
sparis4 <sparis4@msudenver.edu>
2022-06-03 21:38:53 -0600

    correcting syntax

--- demo.sh
+++ demo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Leveraging gunicorn as the application server (recommended solution over default provided by flask)
-exec gunicorn --certfile cert.pem --keyfile priv_key.pem --chdir demo  -b :5000 app:demo
+exec gunicorn --certfile cert.pem --keyfile priv_key.pem --chdir demo  -b :5000 app:app
